78cf62439a512fefc0f47bc938c0153821b9d4e5
Hasan Mrisat <hasanmri890@gmail.com>
2025-06-21 08:17:02 +0300

    Update username-rules.py

--- username-rules.py
+++ username-rules.py
@@ -1,12 +1,13 @@
-username = input("Enter Your Username : ")
+while True:  # This creates an infinite loop
+    username = input("Enter Your Username : ")
 # user's username cant be les sthan 8 characters so
-if len(username) < 8: 
-    print("Username should be at least 8 characters long ❌")
+    if len(username) < 8: 
+        print("Username should be at least 8 characters long ❌")
     # user's username cannot contain spaces so
-elif ' ' in username:
-    print("Username should not contain spaces ❌")
-elif not username.isalpha():
-    print("Username should only contain alphabetical characters 🔠")
-else : 
-    print (f"{username} is a valid username ✔")
-    
+    elif ' ' in username:
+        print("Username should not contain spaces ❌")
+    elif not username.isalpha():
+        print("Username should only contain alphabetical characters 🔠")
+    else : 
+        print (f"{username} is a valid username ✔")
+        break
