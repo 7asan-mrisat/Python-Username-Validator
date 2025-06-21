f7ebffb95d068bce93f1bd146b3b26bc17d1c5a2
Hasan Mrisat <hasanmri890@gmail.com>
2025-06-21 06:34:21 +0300

    Add files via upload

---
+++ username-rules.py
@@ -0,0 +1,12 @@
+username = input("Enter Your Username : ")
+# user's username cant be les sthan 8 characters so
+if len(username) < 8: 
+    print("Username should be at least 8 characters long ❌")
+    # user's username cannot contain spaces so
+elif ' ' in username:
+    print("Username should not contain spaces ❌")
+elif not username.isalpha():
+    print("Username should only contain alphabetical characters 🔠")
+else : 
+    print (f"{username} is a valid username ✔")
+    
